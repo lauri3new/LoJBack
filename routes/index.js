a24e55ecec6ce87bce41bd0cf03be52fd095772e
Laurence Newman <lenewman05@gmail.com>
2017-01-22 14:41:42 +0000

    remove unused routes

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const random = require('./random.js');
 const leagueTable = require('./league.js');
-const quiz = require('./quiz.js');
-const shop = require('./shop.js');
-const search = require('./search.js');
-const addPoints = require('./addpoints.js');
-const importProducts = require('../importproducts').importProducts;
+// const quiz = require('./quiz.js');
+// const shop = require('./shop.js');
+// const search = require('./search.js');
+// const addPoints = require('./addpoints.js');
+// const importProducts = require('../importproducts').importProducts;
 
 const router = express.Router();
-
+// v1 routes
 router.use('/api/league', leagueTable);
 
 router.use('/api/random', random);
 
-router.use('/api/quiz', quiz);
-
-router.use('/api/shop', shop);
-
-router.use('/api/addpoints', addPoints);
-
-router.use('/api/search', search);
-
-router.get('/admin/import', importProducts);
+// router.use('/api/quiz', quiz);
+//
+// router.use('/api/shop', shop);
+//
+// router.use('/api/addpoints', addPoints);
+//
+// router.use('/api/search', search);
+//
+// router.get('/admin/import', importProducts);
 
 module.exports = router;
